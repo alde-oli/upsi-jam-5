afa1951b3293a74d35d3596367b8d473f2d171ea
PantoufleHub <perronalexis2002@gmail.com>
2025-03-16 10:40:16 +0100

    Pouet pouet

--- Scripts/finish_object.gd
+++ Scripts/finish_object.gd
@@ -2,9 +2,5 @@ extends Area2D
 
 func _on_body_entered(body):
 	if body is Player:
-<<<<<<< HEAD
 		get_tree().change_scene_to_file("res://Scenes/WinMenu.tscn")
-=======
 		Son.play_sfx("death")
-		get_tree().change_scene_to_file("res://Scenes/Finish.tscn")
->>>>>>> cf884ca (sound)
