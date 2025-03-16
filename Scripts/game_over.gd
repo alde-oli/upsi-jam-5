a5643c4f95f0b877bbb4eb804026c71bb1a69f3b
PantoufleHub <perronalexis2002@gmail.com>
2025-03-16 11:54:09 +0100

    saoeiudfh

--- Scripts/game_over.gd
+++ Scripts/game_over.gd
@@ -4,7 +4,7 @@ func _ready() -> void:
 	$"YesButton".grab_focus()
 
 func _on_yes_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://Levels/level_1.tscn")
+	get_tree().change_scene_to_file("res://Levels/level_" + str(GameData.current_level) + ".tscn")
 
 func _on_no_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Menu.tscn")
