7f98b4ea236d8e9b59eb41f13f71cb6e8d274e97
Theo Chance <theochance2002@gmail.com>
2025-03-16 05:54:57 +0100

    fixed player colision with spike

--- Scripts/Player/player.gd
+++ Scripts/Player/player.gd
@@ -76,7 +76,7 @@ func take_damage():
 	update_life_ui()
 	if life < 1:
 		print(str(life))
-		get_tree().change_scene_to_file("res://Scenes/Game_over_test.tscn")
+		get_tree().change_scene_to_file("res://Scenes/GameOver.tscn")
 
 func _physics_process(delta):
 	if is_clone_active:
