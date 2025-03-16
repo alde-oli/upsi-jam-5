7af1192ceb6dbf894339130bc2de98e984d411c7
RaphyStoll <raphael.ferreira@ik.me>
2025-03-16 09:52:53 +0100

    sound

--- Scripts/level_1.gd
+++ Scripts/level_1.gd
@@ -1,7 +1,12 @@
 
 extends Node2D
+var flag = 0
 
 func _process(_delta: float) -> void:
+	if flag == 0:
+		flag = 1
+		await get_tree().create_timer(2.0)
+		Son.update_music()
 	if (Input.is_action_just_pressed("Pause")):
 		pauseMenu()
 		
